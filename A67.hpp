13e70b7c8d137c1c857dc854ed26ad239a16e46d
SangaviSuresh <111937090+SangaviSuresh@users.noreply.github.com>
2022-10-18 22:39:19 +0000

    commit

--- A67.hpp
+++ A67.hpp
@@ -1,9 +1,50 @@
-//
-
 #include <iomanip>
 #include <iostream>
 using namespace std;
 
-// complete the Overloading function swap( )
-// swap ( ) function has the difference number of parameters from 2 to 4
-// For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
+//swap function that swaps a & b
+void swap(int &A, int &B)
+{
+    int temp = A;
+    A = B;
+    B = temp;
+}
+
+//swap function that swaps a, b, & c
+void swap(int &a, int &b, int &c)
+{
+    swap(a, b);
+    swap(a, c);
+
+}
+
+//swap function that swaps a, b, c & d
+void swap(int &a, int &b, int &c, int &d)
+{
+    swap(a, b);
+    swap(b, c);
+    swap(c, d);
+
+}
+
+// Get inputs from user
+void getinput(int& num)
+{
+    cout << "Enter value: ";
+    cin >> num;
+}
+void getinput(int& n1, int& n2)
+{
+    cout << "Enter two values: ";
+    cin >> n1 >> n2;
+}
+void getinput(int& n1, int& n2, int& n3)
+{
+    cout << "Enter three values: ";
+    cin >> n1 >> n2 >> n3;
+}
+void getinput(int& n1, int& n2, int& n3, int& n4)
+{
+    cout << "Enter four values: ";
+    cin >> n1 >> n2 >> n3 >> n4;
+}
